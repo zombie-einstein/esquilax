570e36afaca0b54a3784e0b0d1fa8a507b98550c
zombie-einstein <13398815+zombie-einstein@users.noreply.github.com>
2024-09-26 14:21:06 +0100

    Train Test Functionality (#19)

--- src/esquilax/transforms/__init__.py
+++ src/esquilax/transforms/__init__.py
@@ -5,6 +5,6 @@ The transformation implement map-reduce patterns that represent
 agents/entities observing other agents/entities, and functions
 that update the state of agents/entities.
 """
-from ._graph import edge_map, graph_reduce, highest_weight, random_neighbour
+from ._graph import edge_map, graph_reduce, highest_weight, random_edge
 from ._map import amap
 from ._space import nearest_neighbour, spatial
