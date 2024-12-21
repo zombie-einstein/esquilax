d85f03c8ed8869ed534cf711c796e710d0b19189
zombie-einstein <13398815+zombie-einstein@users.noreply.github.com>
2024-12-21 14:29:32 +0000

    Add docstrings

--- src/esquilax/reductions.py
+++ src/esquilax/reductions.py
@@ -1,3 +1,6 @@
+"""
+Reduction interface and common implementations
+"""
 from typing import Callable, Sequence
 
 import chex
@@ -9,29 +12,125 @@ from esquilax.typing import TypedPyTree
 
 
 class Reduction:
+    """
+    Class wrapping a reduction function and its default/identity value
+
+    A reduction function and its associated default value. The
+    reduction function and identity can also be PyTrees to
+    allow reduction of multiple values in a single pass.
+
+    Attributes
+    ----------
+    fn
+        Binary reduction function(s), i.e. this function should
+        take 2 values/arrays and return a value of the
+        same type and shape.
+    id
+        Identity/default value(s), returned if no values are
+        to be reduced.
+    """
+
     __slots__ = ["fn", "id"]
 
-    def __init__(self, fn: TypedPyTree[Callable | ufunc], id: chex.Numeric) -> None:
+    def __init__(self, fn: TypedPyTree[Callable | ufunc], id: chex.ArrayTree) -> None:
+        """
+        Initialise a reduction
+
+        Parameters
+        ----------
+        fn
+            Binary reduction function.
+        id
+            Identity/default value of the reduction.
+        """
         chex.assert_trees_all_equal_structs(
             fn, id
-        ), "Reduction and default PyTrees should have the same structure"
+        ), "'fn' and 'id' PyTrees should have the same structure"
         self.fn = fn
         self.id = id
 
 
 def add(shape: Sequence[int] = (), dtype: jax.typing.DTypeLike = float) -> Reduction:
+    """
+    Addition reduction
+
+    Reduction that adds values, with a default value of ``0``.
+
+    Parameters
+    ----------
+    shape
+        Shape of reduced values.
+    dtype
+        Dtype of reduced values.
+
+    Returns
+    -------
+    Reduction
+        Initialised :py:class:`Reduction`.
+    """
     return Reduction(fn=jnp.add, id=jnp.zeros(shape, dtype=dtype))
 
 
 def logical_or(shape: Sequence[int] = ()) -> Reduction:
+    """
+    Logical or reduction
+
+    Reduction that applies the or function to values, with a default value of ``False``.
+
+    Parameters
+    ----------
+    shape
+        Shape of reduced values.
+
+    Returns
+    -------
+    Reduction
+        Initialised :py:class:`Reduction`.
+    """
     return Reduction(fn=jnp.logical_or, id=jnp.zeros(shape, dtype=bool))
 
 
 def min(shape: Sequence[int] = (), dtype: jax.typing.DTypeLike = float) -> Reduction:
+    """
+    Minimum reduction
+
+    Reduction that returns the minimum of values, with a default value
+    of the maximum numerical values of the dtype.
+
+    Parameters
+    ----------
+    shape
+        Shape of reduced values.
+    dtype
+        Dtype of reduced values.
+
+    Returns
+    -------
+    Reduction
+        Initialised :py:class:`Reduction`.
+    """
     val = jnp.finfo(dtype).max
     return Reduction(fn=jnp.min, id=jnp.full(shape, val, dtype=dtype))
 
 
 def max(shape: Sequence[int] = (), dtype: jax.typing.DTypeLike = float) -> Reduction:
+    """
+    Maximum reduction
+
+    Reduction that returns the maximum of values, with a default value
+    of the minimum numerical values of the dtype.
+
+    Parameters
+    ----------
+    shape
+        Shape of reduced values.
+    dtype
+        Dtype of reduced values.
+
+    Returns
+    -------
+    Reduction
+        Initialised :py:class:`Reduction`.
+    """
     val = jnp.finfo(dtype).min
     return Reduction(fn=jnp.max, id=jnp.full(shape, val, dtype=dtype))
